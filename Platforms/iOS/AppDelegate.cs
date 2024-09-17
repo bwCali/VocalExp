c35435e858e722c74b1360833b5cdd1df667258c
bwCali <bhwftk@gmail.com>
2024-09-16 22:56:27 -0700

    Comparing string arrays
    The challenge:  Compare a static string array with a string captured by speech to text and change the color of words that match.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MoMo4;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
